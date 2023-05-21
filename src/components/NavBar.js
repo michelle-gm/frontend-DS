9fb1968d1b4bf66be1ffb1729002cee2207c54de
michelle-gm <gutierrezmarchena1702@gmail.com>
2023-05-21 13:19:29 -0500

    NavBar ready

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,50 +1,73 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/img/logo.jpeg";
 
 export const NavBar = () => {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, seScrolled] = useState(false);
-    
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 50){
-                seScrolled(true);
-            } else {
-                seScrolled(false);
-            }
-        }
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, seScrolled] = useState(false);
 
-        window.addEventListener("scroll", onScroll);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        seScrolled(true);
+      } else {
+        seScrolled(false);
+      }
+    };
 
-        return () => window.removeEventListener("scroll",onScroll);
-    }, [])
+    window.addEventListener("scroll", onScroll);
 
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
-    }
-    
-    return(
-        <Navbar expand="lg" className={scrolled ? "scrolled":""}>
-      <Container>
-        {/* En esta parte es preferible poner una imagen para hacerlo mas similar al boceto */}
-        <Navbar.Brand href="#home">
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+        <Container>
+          <Navbar.Brand href="#home">
             <div class="logo-image">
-                <img src={logo} alt="Logo" height="100"/>
+              <img src={logo} alt="Logo" height="100" />
             </div>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            {/* <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link> */}
-            <Nav.Link href="#info" className={activeLink === 'info' ? 'active navbar-link': 'navbar-link'} onClick={()=> onUpdateActiveLink('info')}>¿Quiénes somos?</Nav.Link>
-            <Nav.Link href="#login" className={activeLink === 'login' ? 'active navbar-link': 'navbar-link'} onClick={()=> onUpdateActiveLink('login')}>Log in</Nav.Link>
-            <Nav.Link href="#signup" className={activeLink === 'signup' ? 'active navbar-link': 'navbar-link'} onClick={()=> onUpdateActiveLink('signup')}>Sign Up</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="basic-navbar-nav" class="topnav-right">
+            <Nav className="me-auto">
+              {/* <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link': 'navbar-link'} onClick={()=> onUpdateActiveLink('home')}>Home</Nav.Link> */}
+              <Nav.Link
+                href="#info"
+                className={
+                  activeLink === "info" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("info")}
+              >
+                ¿Quiénes somos?
+              </Nav.Link>
+              <Nav.Link
+                href="#login"
+                className={
+                  activeLink === "login" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("login")}
+              >
+                Log in
+              </Nav.Link>
+              <Nav.Link
+                href="#signup"
+                className={
+                  activeLink === "signup" ? "active navbar-link" : "navbar-link"
+                }
+                onClick={() => onUpdateActiveLink("signup")}
+              >
+                Sign Up
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
     </Navbar>
-    );
-}
+  );
+};
