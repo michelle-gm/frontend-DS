213c4d128f02b25095225f8e31709973800173b3
michelle-gm <gutierrezmarchena1702@gmail.com>
2023-05-22 20:16:45 -0500

    Info ready; Issue with footer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
+import { Info } from './components/Info';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Banner/>
+      <Info/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Facebook,Instagram,Linkedin } from "react-bootstrap-icons";
+import logo from "../assets/img/logo.jpeg";
+
+export const Footer = () => {
+  <footer className="footer">
+    <Container>
+      <Row className="align-items-center">
+        <Col sm={6}>
+            <div className="logo-image">
+                <img src={logo} alt="Logo" height="100" />
+            </div>
+        </Col>
+        <Col sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+                <p><Facebook size={25}/></p>
+                <p><Instagram size={25}/></p>
+                <p><Linkedin size={25}/></p>
+            </div>
+        </Col>
+      </Row>
+    </Container>
+  </footer>;
+};
